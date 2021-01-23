df35f3a02e8f4c0c409705da3a924dc89f983c5c
imPrxqx <77047836+imPrxqx@users.noreply.github.com>
2021-01-23 18:16:18 +0100

    Book a Author

---
+++ Models/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectLibri.Models
+{
+    public class Author
+    {
+
+        public String id { get; set; }
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String pseuodonym { get; set; }
+        public String nationality { get; set; }
+        public String sex { get; set; }
+
+    }
+
+}
---
+++ Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectLibri.Models
+{
+    public class Book : Author
+    {
+
+        public String id { get; set; }
+        public String name { get; set; }
+        public String litType { get; set; }
+        List<string> genres = new List<string>();
+        public int releaseYear { get; set; }
+        public String litPeriod { get; set; }
+        public String ISBN { get; set; }
+        public Author Author { get; set; }
+        List<string> litFunctions = new List<string>();
+        public int inventoryCount { get; set; }
+        public double purchasePrice { get; set; }
+
+
+
+    }
+}
