a7ccddf06434b3abe68d87e2be14af9070817afb
VaclavPilat <77143659+VaclavPilat@users.noreply.github.com>
2021-01-28 19:08:47 +0100

    Create NotificationsViewComponent.cs

---
+++ Components/NotificationsViewComponent.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjectLibri.Models;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ProjectLibri.Components
+{
+    public class NotificationsViewComponent : ViewComponent
+    {
+
+        /*public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return;
+        }*/
+
+    }
+}
