639f6b2bc0797f85e91c1d87a4db6e78f1b2bfb1
Dima <dmitryl0626@gmail.com>
2021-02-10 21:04:19 +0100

    ukazka nacitani knih pres controller

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectLibri.Models;
+using ProjectLibri.Service;
+
+namespace ProjectLibri.Controllers
+{
+    public class BooksController : Controller
+    {
+        public JsonResult All()
+        {
+            BookService bookService = new BookService();
+            List<Book> books = bookService.getAll();
+
+            return Json(books);
+        }
+
+        public JsonResult One(String newId)
+        {
+            BookService bookService = new BookService();
+            Book book = bookService.getOne();
+            book.id = newId;
+
+            return Json(book);
+        }
+
+        public JsonResult Index()
+        {
+            return All();
+        }
+
+    }
+}
--- Service/BookService.cs
+++ Service/BookService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectLibri.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,28 @@ namespace ProjectLibri.Service
 {
     public class BookService
     {
+        public List<Book> getAll()
+        {
+            List<Book> books = new List<Book>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Book book = new Book();
+                book.name = "Name" + i;
+                book.id = "id" + i;
+                books.Add(book);
+            }
+
+            return books;
+        }
+
+        public Book getOne()
+        {
+            Book book = new Book();
+            book.name = "Name";
+            book.id = "id";
+
+            return book;
+        }
     }
 }
